5932b5298842bdebe2fc108568a58a3ba400fc2b
GOD OF THUNDER <93386525+yadhuhari@users.noreply.github.com>
2025-03-09 17:36:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+from aiohttp import web as webserver
+
+routes = webserver.RouteTableDef()
+
+async def bot_run():
+    _app = webserver.Application(client_max_size=30000000)
+    _app.add_routes(routes)
+    return _app
+  
+@routes.get("/", allow_head=True)
+async def root_route_handler(request):
+    return webserver.json_response("GOUTHAMSER")
+
+
+
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
